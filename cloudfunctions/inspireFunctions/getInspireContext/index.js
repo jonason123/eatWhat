f2d458e36b26a97364c5c39c2a68c48b5f6b8200
Jonason Jiayuan Zhang (PSP) <jonason.zhang@starbucks.cn>
2022-07-27 09:40:09 +0800

    init cloud function

--- cloudfunctions/inspireFunctions/getInspireContext/index.js
+++ cloudfunctions/inspireFunctions/getInspireContext/index.js
@@ -1,2 +1,11 @@
 exports.main = async (event, context) => {
+  var date = new Date();
+  var hour = date.getHours();
+  var inspireContext = '等会儿吃什么';
+  if (hour < 9 && hour > 6){
+    inspireContext = '早饭吃什么';
+  }
+  return {
+    inspireContext
+  }
 }
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -2,4 +2,19 @@
 // const app = getApp()
 
 Page({
-});
+  data:{
+    helloworld:'你好'
+  },
+  onLoad(){
+    wx.cloud.callFunction({
+      name: 'inspireFunctions',
+      data: {
+        type: 'getInspireContext'
+      }
+    }).then(({result})=>{
+      this.setData({
+        helloworld:result.inspireContext
+      })
+    })
+  }
+})
